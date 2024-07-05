e42915dd20684ce5871f9a1a30d8c6130b82564c
BhavanaAtGit <bs@somaiya.edu>
2024-07-06 00:24:39 +0530

    Modifications

--- server/index.js
+++ server/index.js
@@ -21,7 +21,12 @@ app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+// mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+//     .catch((error) => console.log(error.message));
+
+mongoose.connect(process.env.CONNECTION_URL, {
+})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.error(`Error connecting to MongoDB: ${error.message}`));
 
